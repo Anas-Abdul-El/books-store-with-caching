9ab07ed918d34c20404a2d3aa7908be874aca902
anas abdul el <anasabdulel@gmail.com>
2026-09-01 08:56:55 +0300

    doc: adding some commants and docs to project

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+const userRouter: Router = Router();
+
+// login route
+userRouter.post("/login", (req, res) => {});
+
+// register route
+
+// refresh token route
+
+// logout route
+
+// verify account route
+
+// password reset route
+
+// get user profile route
+
+// update user profile route
+
+export default userRouter;
