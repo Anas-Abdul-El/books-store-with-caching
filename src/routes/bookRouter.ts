d0a7fbe80955eb055b88acbd26d4ce90ef516fa0
anas abdul el <anasabdulel@gmail.com>
2026-09-13 12:15:05 +0300

    book router: adding the router def by commants

--- src/routes/bookRouter.ts
+++ src/routes/bookRouter.ts
@@ -2,4 +2,14 @@ import { Router } from "express";
 
 const bookRouter: Router = Router();
 
+// get books by its id " /book "
+
+// get all books routes with filters and sorting " /books?filter=&sort=&limit= "
+
+// add book " /book/:id "
+
+// update book " /book/:id "
+
+// delete book " /book/:id "
+
 export default bookRouter;
